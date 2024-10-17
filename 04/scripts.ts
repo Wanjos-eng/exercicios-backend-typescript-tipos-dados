ebb88ddc1417bda5eeb39aeb30e2076c7257fa34
Weslen Anjos <weslen_anjos@outlook.com>
2024-10-17 20:03:54 -0300

    Conc: Exercicio 4

---
+++ 04/scripts.ts
@@ -0,0 +1,20 @@
+const geradorEtiqueta = (info: {
+    produto: string,
+    lote: number, 
+    ano: number, 
+    qtd: number
+}): string[] => {
+    const resultado: string[] = [];
+
+    for (let i = 1; i <= info.qtd; i++) {
+        resultado.push(`${info.lote} - ${info.ano} - ${i.toString().padStart(3, '00')}`);
+    }
+    return resultado;
+};
+
+console.log(geradorEtiqueta({
+    produto: 'CPU Dual Core 3.0GHZ',
+    lote: 321,
+    ano: 2022,
+    qtd: 5
+}));
